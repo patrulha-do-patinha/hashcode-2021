ee5d7b2b3bc79093902db22318e749f5c5306480
Pablo Tato Ramos <pabtatoramos@gmail.com>
2021-02-25 20:22:02 +0100

    HI GUYS

--- traffic.py
+++ traffic.py
@@ -34,11 +34,9 @@ class Intersection:
         else:
             self.counts[street] += 1
 
-uniques = set()
 intersections = dict()
 for car in cars:
     for street in car.path:
-        uniques.add(street)
         end = streets[street].end
         if end not in intersections:
             intersections[end] = Intersection(dict())
@@ -50,13 +48,33 @@ def find_gcd(list):
     x = reduce(gcd, list)
     return x
 
-print(len(intersections))
+# print(len(intersections))
+# for i, intersection in intersections.items():
+#     print(i)
+#     print(len(intersection.counts.items()))
+#     street_names = list(intersection.counts.keys())
+#     street_values = [ streets[k].time for k in street_names ]
+#     this_gcd = find_gcd(street_values)
+#     street_values = [ v/this_gcd for v in street_values ]
+#     biggest_time = max(street_values)
+#     for i in range(len(street_names)):
+#         print(f"{street_names[i]} {int(max(biggest_time//street_values[i], 1))}")
+
+# print(len(intersections))
+# for i, intersection in intersections.items():
+#     print(i)
+#     print(len(intersection.counts.items()))
+#     this_gcd = find_gcd(intersection.counts.values())
+#     for (street, n) in intersection.counts.items():
+#         print(f"{street} {n//this_gcd}")
+
+print(len(intersections.items()))
 for i, intersection in intersections.items():
     print(i)
     print(len(intersection.counts.items()))
-    this_gcd = find_gcd(intersection.counts.values())
-    for (street, n) in intersection.counts.items():
-        print(f"{street} {n//this_gcd}")
+    streets = sorted(intersection.counts.keys(), key=lambda x: intersection.counts[x])
+    for (i, street) in enumerate(streets):
+        print(f"{street} {i+1}")
 
 
 
